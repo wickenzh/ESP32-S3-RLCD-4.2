9872e6a25d2ed839f280d3650b0913ee40c279fb
admin <admin@local>
2026-06-14 22:53:22 +0800

    v1.0.11 调整整点报时音频播放链路

--- RLCD_CLOCK/components/port_bsp/codec_bsp.cpp
+++ RLCD_CLOCK/components/port_bsp/codec_bsp.cpp
@@ -17,7 +17,7 @@ void CodecPort::CodecPort_MusicTask(void *arg) {
 		size_t bytes_write = 0;
   	  	size_t bytes_sizt = hourly_chime_pcm_end - hourly_chime_pcm_start;
   	  	uint8_t *data_ptr = (uint8_t *)hourly_chime_pcm_start;
-		codec->CodecPort_SetInfo("es8311",1,44100,2,16);
+		codec->CodecPort_SetInfo("es8311",1,24000,2,16);
 		do
 		{
 			codec->CodecPort_PlayWrite(data_ptr, 256);
@@ -132,7 +132,6 @@ void CodecPort::CodecPort_SetInfo(const char *strName,int open_en,int sample_rat
     	fs.sample_rate = sample_rate;
     	fs.channel = channel;
     	fs.bits_per_sample = bits_per_sample;
-        fs.mclk_multiple = 384;
 	if(open_en) {
         if (!initialized) return;
 		if(!strcmp(strName,"es8311")) {
@@ -156,7 +155,7 @@ bool CodecPort::CodecPort_PlayHourlyChime(void) {
         return false;
     }
     CodecPort_SetSpeakerVol(90);
-    CodecPort_SetInfo("es8311", 1, 44100, 2, 16);
+    CodecPort_SetInfo("es8311", 1, 24000, 2, 16);
     const size_t bytes_size = hourly_chime_pcm_end - hourly_chime_pcm_start;
     const uint8_t *data_ptr = hourly_chime_pcm_start;
     size_t bytes_written = 0;
